8dcbef3af6bd17c040e3d6abe9f3474bf5013c76
gianniskts <trial.dev@parkzenapp.com>
2024-06-27 14:49:31 +0300

    Refactor test cases in test_collector.py and test_medic.py

--- 03_Code/test_collector.py
+++ 03_Code/test_collector.py
@@ -1,23 +1,19 @@
 import pytest
 from collector import Collector, Resource
 
-class TestCollector(pytest.TestCase):
-    def test_track_resource_collection(self):
-        collector = Collector()
-        resource = Resource("Plant_test", 5)
-        collector.track_resource_collection(resource)
-        self.assertEqual(len(collector.resources_collected), 1)
+def test_track_resource_collection():
+    collector = Collector()
+    resource = Resource("Plant_test", 5)
+    collector.track_resource_collection(resource)
+    assert len(collector.resources_collected) == 1
 
-    def test_get_resource_statistics(self):
-        collector = Collector()
-        resource1 = Resource("test1", 5)
-        resource2 = Resource("test2", 3)
-        collector.track_resource_collection(resource1)
-        collector.track_resource_collection(resource2)
-        stats = collector.get_resource_statistics()
-        self.assertEqual(stats.total_resources_collected, 8)
-        self.assertEqual(stats.resource_breakdown["test1"], 5)
-        self.assertEqual(stats.resource_breakdown["test2"], 3)
-
-if __name__ == '__main__':
-    pytest.main()
+def test_get_resource_statistics():
+    collector = Collector()
+    resource1 = Resource("test1", 5)
+    resource2 = Resource("test2", 3)
+    collector.track_resource_collection(resource1)
+    collector.track_resource_collection(resource2)
+    stats = collector.get_resource_statistics()
+    assert stats.total_resources_collected == 8
+    assert stats.resource_breakdown["test1"] == 5
+    assert stats.resource_breakdown["test2"] == 3
--- 03_Code/test_medic.py
+++ 03_Code/test_medic.py
@@ -1,19 +1,15 @@
 import pytest
 from medic import Medic, Player
 
-class TestMedic(pytest.TestCase):
-    def test_add_healing_record(self):
-        medic = Medic()
-        player = Player()
-        medic.add_healing_record(player)
-        self.assertEqual(len(medic.get_healing_history()), 1)
+def test_add_healing_record(self):
+    medic = Medic()
+    player = Player()
+    medic.add_healing_record(player)
+    self.assertEqual(len(medic.get_healing_history()), 1)
 
-    def test_increase_energy(self):
-        player = Player()
-        initial_energy = player.energy[0]
-        medic = Medic()
-        medic.increase_energy(player)
-        self.assertEqual(player.energy[0], initial_energy + 1)
-
-if __name__ == '__main__':
-    pytest.main()
+def test_increase_energy(self):
+    player = Player()
+    initial_energy = player.energy[0]
+    medic = Medic()
+    medic.increase_energy(player)
+    self.assertEqual(player.energy[0], initial_energy + 1)
