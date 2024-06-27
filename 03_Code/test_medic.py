b1c7c7f07189732ad155f6dcdefaced7bc5cf069
gianniskts <trial.dev@parkzenapp.com>
2024-06-27 14:53:47 +0300

    Refactor test_medic.py: Simplify test methods

--- 03_Code/test_medic.py
+++ 03_Code/test_medic.py
@@ -1,15 +1,15 @@
 import pytest
 from medic import Medic, Player
 
-def test_add_healing_record(self):
+def test_add_healing_record():
     medic = Medic()
     player = Player()
     medic.add_healing_record(player)
-    self.assertEqual(len(medic.get_healing_history()), 1)
+    assert len(medic.get_healing_history()) == 1
 
-def test_increase_energy(self):
+def test_increase_energy():
     player = Player()
     initial_energy = player.energy[0]
     medic = Medic()
     medic.increase_energy(player)
-    self.assertEqual(player.energy[0], initial_energy + 1)
+    assert player.energy[0] == initial_energy + 1
